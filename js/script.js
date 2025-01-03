48e44d95c30c8f8ea279e9bdbe776a69d84119db
Giovanni Marcccini <prod.marcaccini@hotmail.com>
2025-01-03 18:53:03 -0300

    commit master
    mutias coisas

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//Ativar Links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+//Ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProdutos);
+
+//perguntas frequnetes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(controls);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+console.log(perguntas);
+
+//Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
